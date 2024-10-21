8b4339e48a0f29a77a23fe618b60f82eada986b2
tzm <89303510+tzm224@users.noreply.github.com>
2024-10-22 00:46:08 +0800

    Add files via upload

---
+++ src/Exp4/data_util.py
@@ -0,0 +1,153 @@
+import torch
+import pandas as pd
+from collections import Counter
+import numpy as np
+from sklearn.model_selection import train_test_split
+from torch.utils.data import Dataset
+
+fit_params = {
+    2: (7.475148902379004, 1.3884478861522933),
+    3: (5.686819960670556, 99.35526026933732),
+    4: (5.419747913747912, 175.14669422777405)
+}
+
+def trendline_func(m_z, a, b, charge):
+    return a * charge * np.sqrt(m_z) + b
+
+def predict_ccs(m_z, charge):
+    m_z = np.array(m_z).reshape(-1)
+    charge = np.array(charge).reshape(-1)
+    predicted_ccs = np.zeros_like(m_z)
+    
+    for chg, (a, b) in fit_params.items():
+        mask = (charge == chg)
+        if mask.any():
+            predicted_ccs[mask] = trendline_func(m_z[mask], a, b, chg)
+    
+    return predicted_ccs
+
+class polypeptide(Dataset):
+    def __init__(self, seq, ccs, charge, length, mz, ccs2, vector):
+        self.seq = torch.LongTensor(seq.tolist())
+        self.ccs = torch.FloatTensor(ccs)
+        self.ccs2 = torch.FloatTensor(ccs2)
+        self.charge = torch.LongTensor(charge)
+        self.length = torch.LongTensor(length)
+        self.mz = torch.FloatTensor(mz)
+        self.vector = torch.FloatTensor(vector.tolist())
+        
+    def __getitem__(self, idx):
+        return self.seq[idx], self.ccs[idx], self.charge[idx], self.length[idx], self.mz[idx], self.ccs2[idx], self.vector[idx]
+    
+    def __len__(self):
+        return len(self.seq)
+
+def process_seq(data, max_length = 64):
+    char_to_int = {char: idx + 6 for idx, char in enumerate(sorted(set(''.join(data['Sequence']))))}
+    char_to_int['<PAD>'] = 0
+    char_to_int['<SOS>'] = 1
+    char_to_int['<EOS>'] = 2
+    char_to_int['Charge2'] = 3
+    char_to_int['Charge3'] = 4
+    char_to_int['Charge4'] = 5
+
+    def encode_and_pad(row):
+        seq, charge = row['Sequence'], row['Charge']
+        charge_token = char_to_int[f'Charge{charge}']
+        sos = char_to_int['<SOS>']
+        eos = char_to_int['<EOS>']
+        encoded = [sos] + [charge_token] + [char_to_int[char] for char in seq] + [eos]
+        padded = np.pad(encoded, (0, max_length - len(encoded)), mode='constant', constant_values=char_to_int['<PAD>'])
+        return padded
+
+    seq = data.apply(encode_and_pad, axis = 1)
+    return np.array(seq.tolist(), dtype = np.int64)
+
+def max_min_transformer(arr):
+    max_val, min_val = np.max(arr), np.min(arr)
+    normalized_arr = (arr - min_val) / (max_val - min_val)
+    return normalized_arr, max_val, min_val
+
+def denormalize_array(normalized_arr, max_val, min_val):
+    denormalized_arr = normalized_arr * (max_val - min_val) + min_val
+    return denormalized_arr
+
+def sequence_to_vector(sequence):
+    valid_amino_acids = ['A', 'R', 'N', 'D', 'C', 'Q', 'E', 'G', 'H', 'I', 
+                         'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V']
+
+    vector = np.zeros(len(valid_amino_acids), dtype = int)
+    
+    counter = Counter(sequence)
+    
+    for char, count in counter.items():
+        if char in valid_amino_acids:
+            index = valid_amino_acids.index(char)
+            vector[index] = count
+    
+    return vector
+
+def charge_to_onehot(charge, num_classes=3):
+    one_hot = np.zeros(num_classes, dtype=int)
+    one_hot[charge - 2] = 1  # 假设电荷状态为2, 3, 4，对应0, 1, 2索引
+    return one_hot
+
+def get_data_set():
+    data = pd.read_csv('../../ttrain.csv')
+    train_data, valid_data = train_test_split(data, test_size = 0.01, random_state = 1115)
+    train_data = train_data.reset_index(drop = True)
+    valid_data = valid_data.reset_index(drop = True)
+    train_seq = process_seq(train_data)
+    valid_seq = process_seq(valid_data)
+    
+    # 将sequence_to_vector应用于每个序列
+    train_vector = np.array([sequence_to_vector(seq) for seq in train_data['Sequence']])
+    valid_vector = np.array([sequence_to_vector(seq) for seq in valid_data['Sequence']])
+    
+    # 将电荷状态转换为独热编码并拼接到vector后面
+    train_onehot = np.array([charge_to_onehot(charge) for charge in train_data['Charge']])
+    valid_onehot = np.array([charge_to_onehot(charge) for charge in valid_data['Charge']])
+    
+    # 拼接电荷状态的独热编码到vector
+    train_vector = np.hstack((train_vector, train_onehot))
+    valid_vector = np.hstack((valid_vector, valid_onehot))
+
+    train_ccs, train_charge = np.array(train_data['CCS']).reshape(-1, 1), np.array(train_data['Charge']).reshape(-1, 1)
+    valid_ccs, valid_charge = np.array(valid_data['CCS']).reshape(-1, 1), np.array(valid_data['Charge']).reshape(-1, 1)
+    train_ccs, valid_ccs = np.log(train_ccs), np.log(valid_ccs)
+
+    train_length = np.array(train_data['Length']) + 3
+    valid_length = np.array(valid_data['Length']) + 3
+    train_mz = np.array(train_data['m/z']).reshape(-1, 1)
+    valid_mz = np.array(valid_data['m/z']).reshape(-1, 1)
+    
+    train_ccs2 = predict_ccs(train_mz, train_charge).reshape(-1, 1)
+    valid_ccs2 = predict_ccs(valid_mz, valid_charge).reshape(-1, 1)
+    train_ccs2, valid_ccs2 = np.log(train_ccs2), np.log(valid_ccs2)
+    
+    train_dataset = polypeptide(train_seq, train_ccs, train_charge, train_length, train_mz, train_ccs2, train_vector)
+    valid_dataset = polypeptide(valid_seq, valid_ccs, valid_charge, valid_length, valid_mz, valid_ccs2, valid_vector)
+    print("Data has been processed!!!")
+
+    return train_dataset, valid_dataset
+
+def get_test_data_set():
+    test_data = pd.read_csv('/root/ttest.csv')
+    test_seq = process_seq(test_data)
+    test_vector = np.array([sequence_to_vector(seq) for seq in test_data['Sequence']])
+    
+    # 将电荷状态转换为独热编码并拼接到vector后面
+    test_onehot = np.array([charge_to_onehot(charge) for charge in test_data['Charge']])
+    test_vector = np.hstack((test_vector, test_onehot))
+
+    test_ccs, test_charge = np.array(test_data['CCS']).reshape(-1, 1), np.array(test_data['Charge']).reshape(-1, 1)
+    test_ccs = np.log(test_ccs)
+    test_length = np.array(test_data['Length']) + 3
+    test_mz = np.array(test_data['m/z']).reshape(-1, 1)
+    
+    test_ccs2 = predict_ccs(test_mz, test_charge).reshape(-1, 1)
+    test_ccs2 = np.log(test_ccs2).reshape(-1, 1)
+    test_dataset = polypeptide(test_seq, test_ccs, test_charge, test_length, test_mz, test_ccs2, test_vector)
+    print("Data has been processed!!!")
+
+    return test_dataset
---
+++ src/Exp4/model.py
@@ -0,0 +1,122 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Mix_Pooling(nn.Module):
+    def __init__(self, embedding_size):
+        super(Mix_Pooling, self).__init__()
+        self.local_agg1 = self._make_cnnblock(embedding_size, embedding_size, 3, 1, 1)
+        self.local_agg2 = self._make_cnnblock(embedding_size, embedding_size, 5, 1, 1)
+        self.local_agg3 = self._make_cnnblock(embedding_size, embedding_size, 7, 1, 1)
+
+        self.global_linear = nn.Linear(embedding_size, 1)
+        
+    def _make_cnnblock(self, in_channels, out_channels, kernel_size, dilation, nlayers):
+        layers = []
+        layers.append(nn.Conv1d(in_channels, out_channels, kernel_size, padding=dilation*int((kernel_size-1)/2), dilation=dilation))
+        layers.append(nn.SELU())
+
+        for _ in range(nlayers - 1):
+            layers.append(nn.Conv1d(out_channels, out_channels, kernel_size, padding=dilation*int((kernel_size-1)/2), dilation=dilation))
+            layers.append(nn.SELU())
+            in_channels = out_channels
+        return nn.Sequential(*layers)
+
+    def forward(self, seq, mask):
+        mask = (~mask).unsqueeze(-1).float()
+        cls = seq[:, 0, :]
+        seq = seq * mask
+        
+        res = self.global_linear(seq)
+        res = torch.where(mask == 0.0, torch.tensor(float('-inf'), device=res.device), res)
+        pooling_weight = F.softmax(res, dim=1)
+        res = torch.sum(pooling_weight * seq, dim=1)
+        
+        seq = seq.permute(0, 2, 1)
+        mask = mask.permute(0, 2, 1)
+        seq = self.local_agg1(seq) * mask
+        seq = self.local_agg2(seq) * mask
+        seq = self.local_agg3(seq) * mask
+        seq = seq.permute(0, 2, 1)
+        mask = mask.permute(0, 2, 1)
+        
+        seq = torch.max(seq, dim=1)
+
+        return torch.cat([res, seq], dim=1)
+
+
+class Embedding(nn.Module):
+    def __init__(self, embedding_size, max_len):
+        super(Embedding, self).__init__()
+        self.max_len = max_len
+        self.embedding_size = embedding_size
+        self.seq_embedding = nn.Embedding(32, embedding_dim=embedding_size)
+        self.position_embedding = nn.Embedding(max_len, embedding_size)
+            
+    def forward(self, seq):
+        batch_size, _  = seq.shape
+        position = torch.arange(0, self.max_len).expand(batch_size, self.max_len).to(seq.device)
+        seq = self.seq_embedding(seq)
+        seq = seq + self.position_embedding(position)
+        return seq
+
+class EncoderLayer(nn.Module):
+    def __init__(self, num_layers, embedding_size, num_heads, dropout):
+        super(EncoderLayer, self).__init__()
+        self.num_layers = num_layers 
+        d_ff = 4 * embedding_size
+        self.attn = nn.ModuleList([nn.MultiheadAttention(embedding_size, num_heads, dropout, batch_first=True)
+                                   for _ in range(num_layers)])
+
+        self.mlp = nn.ModuleList([nn.Sequential(nn.Linear(embedding_size, d_ff), nn.GELU(), nn.Dropout(dropout), nn.Linear(d_ff, embedding_size))
+                                  for _ in range(num_layers)])
+
+        self.norms1 = nn.ModuleList([nn.LayerNorm(embedding_size) for _ in range(num_layers)])
+        self.norms2 = nn.ModuleList([nn.LayerNorm(embedding_size) for _ in range(num_layers)])
+        self.norm3= nn.LayerNorm(embedding_size)
+        
+    def forward(self, seq, mask):
+        for i in range(self.num_layers):
+            seq2 = self.norms1[i](seq)
+            seq2, _ = self.attn[i](seq2, seq2, seq2, key_padding_mask=mask)
+            seq = seq2 + seq
+            
+            seq2 = self.norms2[i](seq)
+            seq2 = self.mlp[i](seq2)
+            seq = seq2 + seq
+            
+        seq = self.norm3(seq)
+        return seq
+
+class Regression(nn.Module):
+    def __init__(self, embedding_size, p):
+        super(Regression, self).__init__()
+        self.mlp = nn.Sequential(
+            nn.Linear(2 * embedding_size + 25, embedding_size),
+            nn.LayerNorm(embedding_size),
+            nn.ReLU(),
+            nn.Linear(embedding_size, 1),
+        )
+    def forward(self, vector, length, seq, mz):
+        length = length.unsqueeze(-1).to(vector.device)
+        final_state = torch.cat([vector, length, seq, mz], dim=1)
+        return self.mlp(final_state)
+
+class Seq2CCS(nn.Module):
+    def __init__(self, num_layers, embedding_size, num_heads, dropout, p, max_len=64):
+        super(Seq2CCS, self).__init__()
+        self.max_len = max_len
+        self.Embedding = Embedding(embedding_size, max_len)
+        self.Encoder = EncoderLayer(num_layers, embedding_size, num_heads, dropout)
+        self.Pooling = Mix_Pooling(embedding_size)
+        self.Regression = Regression(embedding_size, p)
+    
+    def forward(self, seq, charge, length, mz, ccs2, vector):
+        mask = (seq == 0).clone().detach().to(seq.device)
+        seq = self.Embedding(seq)
+        seq = self.Encoder(seq, mask)
+        
+        seq = self.Pooling(seq, mask)
+        out = self.Regression(vector, length, seq, torch.sqrt(mz)) + ccs2
+        return out
+
