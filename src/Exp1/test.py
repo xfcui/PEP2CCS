579cb4047751b69bd09f6905853ee1f74dc948d0
tzm <89303510+tzm224@users.noreply.github.com>
2024-11-10 12:33:41 +0800

    Update test.py

--- src/Exp1/test.py
+++ src/Exp1/test.py
@@ -22,7 +22,7 @@ def main():
     
     data_set = get_test_data_set()
     test_dataloader = DataLoader(data_set, batch_size = batch_size, shuffle = False)
-    device = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
+    device = torch.device('cuda:2' if torch.cuda.is_available() else 'cpu')
     print('device: ', device)
     
     model = PEP2CCS(model_params['num_layers'], model_params['embedding_size'], model_params['num_heads'], 0, 0).to(device)
