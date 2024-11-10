e147a7d6552b4547caca15fc99866ae10d7100a5
tzm <89303510+tzm224@users.noreply.github.com>
2024-11-10 12:42:43 +0800

    Update run.sh

--- src/Exp5/run.sh
+++ src/Exp5/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo "##Exp5: The function of Mix Pooling"
-python /root/PEP2CCS/src/Exp5/test.py 1 /root/PEP2CCS/checkpoint/model1.pt
-python /root/PEP2CCS/src/Exp5/test.py 2 /root/PEP2CCS/checkpoint/model2.pt
-python /root/PEP2CCS/src/Exp5/test.py 3 /root/PEP2CCS/checkpoint/model3.pt
-python /root/PEP2CCS/src/Exp5/test.py 4 /root/PEP2CCS/checkpoint/model4.pt
-python /root/PEP2CCS/src/Exp5/test.py 5 /root/PEP2CCS/checkpoint/model5.pt
-python /root/PEP2CCS/src/Exp5/test.py 6 /root/PEP2CCS/checkpoint/model6.pt
-python /root/PEP2CCS/src/Exp5/test.py 7 /root/PEP2CCS/checkpoint/model7.pt
+echo "##Exp5: The role of Mix Pooling"
+python ./src/Exp5/test.py --model_choice 1 --model_path "./src/Exp5/model1.pt"
+python ./src/Exp5/test.py --model_choice 2 --model_path "./src/Exp5/model2.pt"
+python ./src/Exp5/test.py --model_choice 3 --model_path "./src/Exp5/model3.pt"
+python ./src/Exp5/test.py --model_choice 4 --model_path "./src/Exp5/model4.pt"
+python ./src/Exp5/test.py --model_choice 5 --model_path "./src/Exp5/model5.pt"
+python ./src/Exp5/test.py --model_choice 6 --model_path "./src/Exp5/model6.pt"
+python ./src/Exp5/test.py --model_choice 7 --model_path "./src/Exp5/model7.pt"
 
 echo "##Exp 5 replication completed!"
