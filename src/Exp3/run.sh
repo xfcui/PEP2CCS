2752ec6a666e1533d76604e6d8a3836eb0f58ac0
tzm <89303510+tzm224@users.noreply.github.com>
2024-11-10 12:39:28 +0800

    Create run.sh

---
+++ src/Exp3/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "##Exp3: The role of m/z and length"
+
+python ./src/Exp3/test3.py
+python ./src/Exp3/test2.py
+python ./src/Exp3/test3.py
+
+
+echo "##Exp 3 replication completed!"
