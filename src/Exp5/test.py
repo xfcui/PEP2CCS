254a0e312808bb035ff5264171b07a8608bed85c
tzm <89303510+tzm224@users.noreply.github.com>
2024-11-10 13:06:43 +0800

    Update test.py

--- src/Exp5/test.py
+++ src/Exp5/test.py
@@ -45,7 +45,7 @@ def main():
     batch_size = 1  
     test_dataloader = DataLoader(data_set, batch_size=batch_size, shuffle=False)
     
-    device = torch.device('cuda:2' if torch.cuda.is_available() else 'cpu')
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print('device: ', device)
     
     model = model_class(model_params['num_layers'], model_params['embedding_size'], model_params['num_heads'], 0, 0).to(device)
