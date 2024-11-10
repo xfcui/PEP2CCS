b0be7d00fde939d19dea54c7f26bbafd68129c8c
tzm <89303510+tzm224@users.noreply.github.com>
2024-11-10 12:41:13 +0800

    Update test.py

--- src/Exp4/test.py
+++ src/Exp4/test.py
@@ -1,44 +1,41 @@
+import sys
+import os
 import pandas as pd
 import torch
-from PEP2CCS.src.Exp3.data_util import get_test_data_set
-from PEP2CCS.src.model.model import *
-from PEP2CCS.src.model.model_params import *
+from data_util import *
+from model_params import * 
+from model import *
 from torch.utils.data import DataLoader
 import time
 from tqdm import tqdm
+import matplotlib.pyplot as plt
+import seaborn as sns
 import numpy as np
-import argparse
 
 def main():
-    parser = argparse.ArgumentParser(description="Run the PEP2CCS model with custom model path and data selection.")
-    parser.add_argument('--model_path', type=str, default="/root/PEP2CCS/src/Exp4/model.pt", help='Path to the model checkpoint')
-    args = parser.parse_args()
-    
     print("##model params: {}", model_params)
-    data = pd.read_csv('/root/PEP2CCS/data/test_data.csv')    
+    batch_size = 1
+    data = pd.read_csv('./src/data/test_data.csv')    
     
     data_set = get_test_data_set()
-    
-    test_dataloader = DataLoader(data_set, batch_size=batch_size, shuffle=False)
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    test_dataloader = DataLoader(data_set, batch_size = batch_size, shuffle = False)
+    device = torch.device('cuda:2' if torch.cuda.is_available() else 'cpu')
     print('device: ', device)
     
     model = PEP2CCS(model_params['num_layers'], model_params['embedding_size'], model_params['num_heads'], 0, 0).to(device)
-    
-    model_path = args.model_path
-    chkp = torch.load(model_path)
+    model_path = "./src/Exp4/model.pt" 
+    chkp = torch.load(model_path, map_location = device)
     state_dict = chkp['model_param']
     model.load_state_dict(state_dict)
     
     max_len = 64
-    batch_size = 1
+
     test_size = len(data_set)
     pred_ccs = np.zeros([test_size, 1])
     ccs = np.zeros([test_size, 1])
     charge = np.zeros([test_size, 1])
     seq = np.zeros([test_size, max_len])
     Length = np.zeros([test_size, max_len])
-    max_pos = np.zeros([test_size, max_len])
     i = 0
     start = time.time()
     model.eval()
@@ -46,7 +43,7 @@ def main():
     test_loss = []
     test_dataloader = tqdm(test_dataloader, total=len(test_dataloader), desc="Model on test set", unit='batch')
 
-    for batch_seq, batch_ccs, batch_charge, batch_length, batch_mz, batch_ccs2 in test_dataloader:
+    for batch_seq, batch_ccs, batch_charge, batch_length, batch_mz, batch_ccs2, t_vector in test_dataloader:
         with torch.no_grad():
             batch_seq = batch_seq.to(device)
             batch_ccs = batch_ccs.to(device)
@@ -54,10 +51,10 @@ def main():
             batch_charge = batch_charge.to(device)
             batch_mz = batch_mz.to(device)
             batch_length = batch_length.to(device)
+            t_vector = t_vector.to(device)
             
-            pred, max_indices = model(batch_seq, batch_charge, batch_length, batch_mz, batch_ccs2)
+            pred = model(batch_seq, batch_charge, batch_length, batch_mz, batch_ccs2, t_vector)
             
-            idx = torch.argmax(max_indices, dim = 1)
             pred, batch_ccs = torch.squeeze(pred), torch.squeeze(batch_ccs)
             pred, batch_ccs = torch.exp(pred), torch.exp(batch_ccs)
             
@@ -67,7 +64,6 @@ def main():
             ccs[i:i + batch_size, :] = batch_ccs.cpu().numpy().reshape(-1, 1)
             charge[i:i + batch_size, :] = batch_charge.cpu().numpy().reshape(-1, 1)
             Length[i:i + batch_size, :] = batch_length.cpu().numpy().reshape(-1, 1)
-            max_pos[i:i + batch_size, :] = idx.cpu().numpy().reshape(-1, 1)
 
             seq[i:i + batch_size, :] = batch_seq.cpu().numpy().reshape(-1, max_len)
 
@@ -77,6 +73,7 @@ def main():
     print("##Finish!")
     print(f'##Time: {(time.time() - start) / 60}')
     print(f'##Test Loss: {avr_loss}')
+    
     
     pred_ccs = pred_ccs.ravel()
     data['Pred CCS'] = pred_ccs
@@ -90,6 +87,7 @@ def main():
     mse = ((df['CCS'] - df['Pred CCS']) ** 2).mean()
     print(f'MSE: {mse}')
     print("Mediant Abusolute Relative Error: ", np.median(np.abs(df['Relative deviation'])))
-    
-if __name__ == 'main':
+    df['Error'] = (df['Relative deviation'])
+
+if __name__ == '__main__':
     main()
